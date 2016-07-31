0777e5555e2dd420ca332575b739da55a53e79ab
Aaron Sikes <aaron@sikes.io>
2016-07-31 15:49:06 -0400

    Move UI code to own class

---
+++ js/main.js
@@ -0,0 +1,1 @@
+UI = new GuessUI();
---
+++ js/ui.js
@@ -0,0 +1,42 @@
+function GuessUI() {
+  this.output_frame = document.querySelector('.output-frame')
+  this.input_form = document.querySelector('.input-form')
+  this.input_field = document.querySelector('.guess-input')
+  this.listeners = [];
+
+  this.input_form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    var input = this.input_field.value;
+    this.addPlayerMessage(input);
+    input_field.value = "";
+
+    this.listeners.forEach(function(l) {
+      l(input);
+    });
+  }.bind(this));
+}
+
+GuessUI.prototype.addListener = function(listener) {
+  this.listeners.push(listener);
+};
+
+GuessUI.prototype.addPlayerMessage = function addPlayerMessage(text) {
+  var message = create_message(text);
+  message.classList.add('player');
+  this.output_frame.appendChild(message);
+};
+
+GuessUI.prototype.addComputerMessage = function addPlayerMessage(text) {
+  var message = create_message(text);
+  message.classList.add('computer');
+  this.output_frame.appendChild(message);
+};
+
+function create_message(text) {
+  var new_message = document.createElement('div');
+  new_message.classList.add("message");
+  new_message.innerHTML = text;
+  
+  return new_message;
+}
